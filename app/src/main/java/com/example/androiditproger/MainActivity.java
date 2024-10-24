bb9a73426b987b10d13a91893e389ffa70324cc7
Максим_офис <mrbarawka@gmail.com>
2024-10-24 22:06:45 +0300

    🟩Написано приложение с кнопками и сообщениями

--- app/src/main/java/com/example/androiditproger/MainActivity.java
+++ app/src/main/java/com/example/androiditproger/MainActivity.java
@@ -1,48 +1,70 @@
 package com.example.androiditproger;
 
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    private TextView resultText;
-    private EditText number_field1, number_field2;
-    private Button add_button;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        resultText = findViewById(R.id.resultText);
-        number_field1 = findViewById(R.id.number_field1);
-        number_field2 = findViewById(R.id.number_field2);
-        add_button = findViewById(R.id.add_button);
-
-        add_button.setOnClickListener(new View.OnClickListener() {
+        TextView info = findViewById(R.id.textView);
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                float num1 = Float.parseFloat(number_field1.getText().toString());
-                float num2 = Float.parseFloat(number_field2.getText().toString());
-                float res = num1 + num2;
+            public void onClick(View v) {
+//                showInfo(info.getText().toString(), btn2);
 
-                resultText.setText(String.valueOf(res));
+                showInfoAlert("Вы хотите закрыть приложение?");
             }
         });
+    }
+
+    public void btnClick(View v) {
+        showInfo(((Button) v).getText().toString(), ((Button) v));
+    }
+
+    private void showInfo(String info, Button btn) {
+        btn.setText("Уже нажали");
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.CYAN));
+        Toast.makeText(this, info, Toast.LENGTH_LONG).show();
+    }
+
+    private void showInfoAlert(String info) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Большая подсказка")
+                .setMessage(info)
+                .setCancelable(false)
+                .setPositiveButton("Конечно!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Нет!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
