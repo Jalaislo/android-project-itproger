2dab2c6c93573f84cb40be0b37871204e42665a7
Максим_офис <mrbarawka@gmail.com>
2024-10-25 17:11:48 +0300

    🟩Написано приложение тестирующее изображения

--- app/src/main/java/com/example/androiditproger/MainActivity.java
+++ app/src/main/java/com/example/androiditproger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androiditproger;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void messageTexting (View v) {
+        TextView text = findViewById(R.id.textView);
+
+        text.setText("Я андроид лол!");
     }
 }
